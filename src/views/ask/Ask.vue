46ccbde25192f95e8dabeea0819184ae202872e0
luxinjuan <1769099582@qq.com>
2020-06-27 23:08:18 +0800

    修改图标

--- src/views/ask/Ask.vue
+++ src/views/ask/Ask.vue
@@ -2,8 +2,8 @@
   <div id="ask">
     <h2>提问问题</h2>
     <a-input size="large" placeholder="请输入标题，用问号结尾" />
-    <div id="image">
-      <img src="@/assets/img/icon/标题.png" title="标题" />
+    <!-- <div id="image"> -->
+    <!-- <img src="@/assets/img/icon/标题.png" title="标题" />
       <img src="@/assets/img/icon/加粗.png" title="加粗" />
       <img src="@/assets/img/icon/斜体.png" title="斜体" /> 
       <img src="@/assets/img/icon/删除线.png" title="删除线" />
@@ -16,10 +16,24 @@
      
       <img src="@/assets/img/icon/图片.png" title="图片" />
       <img src="@/assets/img/icon/链接.png" title="链接" />
-      <img src="@/assets/img/icon/代码块.png" title="代码块" />
+    <img src="@/assets/img/icon/代码块.png" title="代码块" />-->
+    <div class="icons-list">
+      <!-- <a-icon type="font-size" @click="fontsize" /> -->
+      <a-icon type="bold" @click="bold" />
+      <a-icon type="italic" />
+      <a-icon type="strikethrough" />
+
+      <a-icon type="align-left" />
+      <a-icon type="ordered-list" />
+      <a-icon type="unordered-list" />
+
+      <a-icon type="code" />
+      <a-icon type="link" />
+      <a-icon type="unordered-list" />
     </div>
+    <!-- </div> -->
     <div>
-      <a-textarea id="textarea" placeholder="提问内容" :rows="10" />
+      <a-textarea id="textarea" v-model="message" placeholder="提问内容" :rows="10" />
     </div>
     <a-divider />
     <a-button id="but" type="primary">发布问题</a-button>
@@ -27,7 +41,17 @@
 </template>
 <script>
 export default {
-  name: "Ask"
+  name: "Ask",
+  data() {
+    return {
+      message: ""
+    };
+  },
+  methods: {
+    bold() {
+      console.log(this.message);
+    }
+  }
 };
 </script>
 <style>
@@ -55,12 +79,26 @@ h2 {
   padding-bottom: 5px;
   border-radius: 5px;
 }
-img {
+/* img {
   height: 16px;
   width: 16px;
   margin-left: 10px;
 }
 img:hover {
-    border: 1px solid #777;
+  border: 1px solid #777;
+} */
+
+.icons-list {
+  margin-top: 20px;
+  background: white;
+  height: 25px;
+  border: 2px slid gray;
+  padding-left: 10px;
+}
+.icons-list >>> .anticon {
+  font-size: 30px;
+}
+.icons-list >>> .anticon:hover {
+  border: 1px solid #777;
 }
 </style>
